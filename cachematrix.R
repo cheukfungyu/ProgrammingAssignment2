0bd0445df7c1f58d1460fe748949e20d12688c44
Marcus Yu <yucheukfung@hotmail.com>
2024-05-26 21:27:28 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -7,8 +7,8 @@ makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
   
   fun1 <- function(y) {
-    x <- y
-    inv <- NULL
+    x <<- y
+    inv <<- NULL
   }
   
   fun2 <- function() {
@@ -23,13 +23,20 @@ makeCacheMatrix <- function(x = matrix()) {
     inv
   }
   
-  return(list(fun1 = fun1, fun2 = fun2, fun3 = fun3, fun4 = fun4))
+  list(fun1 = fun1, fun2 = fun2, fun3 = fun3, fun4 = fun4)
 }
-
-
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverse <- x$fun4()
+  if (!is.null(inverse)) {
+    message("Getting cached data")
+    return(inverse)
+  }
+  
+  data <- x$fun2()
+  inverse <- solve(data, ...)
+  x$fun3(inverse)
+  inverse
 }
