39e71962739852fb4e6ffd76bc366785e73e484d
Marcus Yu <yucheukfung@hotmail.com>
2024-05-26 17:11:51 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,8 +4,28 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-x<-
+  inv <- NULL
+  
+  fun1 <- function(y) {
+    x <- y
+    inv <- NULL
+  }
+  
+  fun2 <- function() {
+    x
+  }
+  
+  fun3 <- function(inverse) {
+    inv <<- inverse
+  }
+  
+  fun4 <- function() {
+    inv
+  }
+  
+  return(list(fun1 = fun1, fun2 = fun2, fun3 = fun3, fun4 = fun4))
 }
+
 
 
 ## Write a short comment describing this function
